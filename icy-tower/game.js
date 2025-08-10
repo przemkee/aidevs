09ac28e657ddf7920ebe7a6075fe932945bc9c5a
Przemyslaw Nowak <przemekno@gmail.com>
2025-08-11 01:15:17 +0200

    fix: restore combo threshold and scale with jump boost

--- icy-tower/game.js
+++ icy-tower/game.js
@@ -104,7 +104,8 @@ function update() {
   else player.vx = 0;
 
   if (keys['Space'] && player.onGround) {
-    player.vy = -20;
+    const heightBoost = comboMultiplier >= 4 ? comboMultiplier / 2 : 1;
+    player.vy = -20 * heightBoost;
     player.onGround = false;
     if (!gameStarted) gameStarted = true;
   }
@@ -132,13 +133,21 @@ function update() {
       player.vy = 0;
       player.onGround = true;
       const jumped = plat.id - player.lastPlatformId;
-      if (jumped >= 3) {
-        comboHits++;
-        if (comboHits >= comboMultiplier) {
-          comboMultiplier *= 2;
-          comboHits = 0;
+      const highestId = platforms[platforms.length - 1].id;
+      const isHighest = plat.id === highestId;
+      if (isHighest) {
+        const heightBoost = comboMultiplier >= 4 ? comboMultiplier / 2 : 1;
+        const needed = 4 * heightBoost;
+        if (jumped >= needed) {
+          comboHits++;
+          if (comboHits >= comboMultiplier) {
+            comboMultiplier *= 2;
+            comboHits = 0;
+          }
+          score += jumped * comboMultiplier;
+        } else {
+          score += jumped;
         }
-        score += jumped * comboMultiplier;
       } else {
         score += jumped;
         comboMultiplier = 1;
